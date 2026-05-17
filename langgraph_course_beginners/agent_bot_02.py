e5991ac63eb315f95a5343080fe4a0205f9b8ef2
DanielDonato <38320247+DanielDonato@users.noreply.github.com>
2026-05-17 19:17:53 -0300

    Add files via upload

---
+++ langgraph_course_beginners/agent_bot_02.py
@@ -0,0 +1,36 @@
+from typing import TypedDict, List, Union
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[Union[HumanMessage, AIMessage]]
+
+llm = ChatGroq(model="llama-3.1-8b-instant")
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"\nAI {response.content}")
+    state["messages"].append(AIMessage(content=response.content))
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+conversation_history = []
+
+user_input = input("Enter your message: ")
+
+while user_input != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result["messages"]
+    user_input = input("Enter your message: ")
+
+
